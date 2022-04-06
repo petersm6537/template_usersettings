aff8400d071bafef7c3b5a546747a8a40739f9f0
Maverick Peters <petersm6537@my.uwstout.edu>
2022-04-06 12:51:10 -0500

    Major working changes 1. Functional directory selection with fileopendialog 2. User able to edit application settings

--- template_usersettings/Program.cs
+++ template_usersettings/Program.cs
@@ -8,15 +8,55 @@ namespace template_usersettings
         [STAThread]
         static void Main()
         {
+            #region Comments
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            // 1. Main program tree
+            #endregion Comments
+
             ApplicationConfiguration.Initialize();
+            establishVariables();
             Application.Run(new usersettings_form());
+
         }
 
-        static void BranchOne()
-        {
 
+
+
+
+        static public void establishVariables()
+        {
+            #region Comments
+            /* 1. Establishes the values of our global variables
+             * 2. Then checks to see if a user editable one has a value
+             */
+            #endregion Comments
+
+            GlobalVariables.defaultfilepath = Properties.Settings.Default.defaultfilepath;
+            GlobalVariables.userfilepath = Properties.Settings.Default.userfilepath;
+
+            if (string.IsNullOrWhiteSpace(GlobalVariables.userfilepath))
+            {
+                GlobalVariables.userfilepath = GlobalVariables.defaultfilepath;
+                Properties.Settings.Default.userfilepath = GlobalVariables.userfilepath;
+                Properties.Settings.Default.Save();
+            }
+
+        }
+
+
+
+
+        static public class GlobalVariables
+        {
+            #region Comments
+            /*All of our global variables are stored here
+             * 
+             */
+            #endregion Comments
+
+            static public string defaultfilepath;
+            static public string userfilepath;
         }
     }
 }
